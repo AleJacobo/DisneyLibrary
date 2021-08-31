d7c8afa19cd6c43abaefde09474ceba76c4eeaeb
Alejandro Jacobo <thesenntinel@outlook.com>
2021-08-31 17:43:34 -0300

    Entity and DB entry on SQL Server /Entities/User dont have any information

---
+++ Disney.Infrastructure/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using Disney.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disney.Infrastructure
+{
+    public class DataContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+        {
+            dbContextOptionsBuilder.UseSqlServer("data source = Localhost; " +
+                                                 "initial catalog = DisneyAlkemyChallenge; " +
+                                                 "Integrated Security = true");
+        }
+
+        public DbSet<Character> Characters { get; set; }
+        public DbSet<MovieSerie> MoviesSeries { get; set;    }
+        public DbSet<Genre> Genres { get;  set; }
+        public DbSet<User> Users { get; set; }
+
+    }
+}
